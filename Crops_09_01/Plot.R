fea496fe9d01b21f3ce7e10c4d5af496a9430181
Spandana Makeneni <makeneni@gmail.com>
2020-09-01 13:59:10 -0400

    Update Plot.R

--- Crops_09_01/Plot.R
+++ Crops_09_01/Plot.R
@@ -11,11 +11,14 @@ USA_long_crops <- na.omit(USA_long_crops)
 
 #stack area plot
 USA_plot <- ggplot(USA_long_crops,aes(x=Year,y=cropquantity,fill=croptype))+geom_area(colour="black",alpha=0.6,size=0.5)+
-  scale_fill_viridis(discrete = T,name="crop type")+theme_ipsum()+ggtitle("Crop Yeild in USA between 1961 and 2018")+
-  scale_x_continuous(breaks=seq(1960,2020,5))+theme(axis.text.x=element_text(angle=45))+theme(plot.title = element_text(hjust=0.5))
+  scale_fill_viridis(discrete = T,name="crop type")+theme_ipsum()+ggtitle("Crop Yields in USA between 1961 and 2018")+
+  scale_x_continuous(breaks=seq(1960,2020,5))+
+  theme(axis.text.x=element_text(face="bold",angle=45),axis.text.y = element_text(face="bold"),axis.title.x = element_text(face="bold",size=15),
+        axis.title.y=element_text(face="bold",size=15))+
+  theme(plot.title = element_text(hjust=0.5))
 
 #saving the plot
-ggsave(plot=USA_plot,filename="USA_plot.png",device="png",dpi=300)
+ggsave(plot=USA_plot,filename="USA_plot.png",device="png",dpi=600)
 
 
 
